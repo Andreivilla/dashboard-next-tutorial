da86da3c60aa5de3df839e489ac34b0cdc52f109
unknown <andreivilla63@gmail.com>
2025-09-09 10:12:10 -0300

    cap 15

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -20,7 +20,6 @@ export async function fetchRevenue() {
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
     //console.log('Data fetch completed after 3 seconds.');
 
     return data;
@@ -159,7 +158,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
